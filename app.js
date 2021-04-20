74c5200fd7b39951ceba8bdc29ffe2cbedcd7cff
ahmed <ahmedahmedmagdy491@gmail.com>
2021-04-20 22:21:57 +0200

    ready for server

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
-const express = require('express')
-require('dotenv/config')
-const path = require('path')
-const app = express()
+const express = require('express');
+require('dotenv/config');
+const path = require('path');
+const app = express();
 
 // View Engine
 app.set('view engine', 'pug');
-// set the views "folder" is the views 
+// set the views "folder" is the views
 app.set('views', 'views');
 
 // Static Files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.listen(process.env.PORT || 5000, () => {
+	console.log('port listen on port', process.env.PORT);
+});
 
-const port = process.env.port || 4000
-
-
-app.listen(port, ()=>{
-    console.log('port listen on port', port)
-})
-
-app.get('/', (req,res)=>{
-    res.render('index')
-})
-app.get('/about', (req,res)=>{
-    res.render('about')
-})
-app.get('/work', (req,res)=>{
-    res.render('work')
-})
-app.get('/contact', (req,res)=>{
-    res.render('contact')
-})
+app.get('/', (req, res) => {
+	res.render('index');
+});
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+app.get('/work', (req, res) => {
+	res.render('work');
+});
+app.get('/contact', (req, res) => {
+	res.render('contact');
+});
